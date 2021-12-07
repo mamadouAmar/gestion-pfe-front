2b667a7da07648bdd40ac5e5fea0ed454e63bdc3
quantumAmar <dmamadouamar@ept.sn>
2021-12-07 22:06:56 +0000

    chore: initialisation des services et des models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
@@ -20,6 +18,8 @@ import { EtudiantComponent } from './component/etudiant/etudiant.component';
 import { ProfesseurComponent } from './component/professeur/professeur.component';
 import { SoutenantComponent } from './component/soutenant/soutenant.component';
 import { GerantPfeComponent } from './component/gerant-pfe/gerant-pfe.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
